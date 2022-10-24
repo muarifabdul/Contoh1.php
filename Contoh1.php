ae9905953336191075dc7ea2a04a268d1c0e5041
WP2_muarif <116548669+muarifabdul@users.noreply.github.com>
2022-10-24 15:21:28 +0700

    Add files via upload

---
+++ Contoh1.php
@@ -0,0 +1,10 @@
+<?php
+class contoh1 extends CI_controller
+{
+    public function index()
+    {
+       echo "<h1>perkenalkan</h1>";
+       echo"Nama saya Muarif Abdul Azis saya tinggal di BSD";
+    
+}
+}
